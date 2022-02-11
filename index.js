e4f7b27c4a332415ff0245cf103420d79ccf6d46
feden2906 <feden2906@gmail.com>
2022-02-11 14:40:00 +0200

    finish

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const fs = require('fs').promises;
 
 fs.readFile('./movies.txt').then(i => {
-  const fields = ['Title', 'Release Year', 'Format', 'Stars']
+  const fields = ['Title', 'Release Year', 'Format', 'Stars'];
   const movies = [];
   let movieBuffer = {};
 
   i.toString().split('\n').forEach(string => {
-
     const arrayFromStr = string.split(': ');
+
     if (arrayFromStr.length <= 1) {
       movies.push(movieBuffer);
       movieBuffer = {};
       return;
     }
+
     if (fields.includes(arrayFromStr[0])) {
-      movieBuffer[arrayFromStr[0].charAt(0).toLowerCase() + arrayFromStr[0].slice(1).split(' ').join('')]  = arrayFromStr[1];
+      movieBuffer[arrayFromStr[0].charAt(0).toLowerCase() + arrayFromStr[0].slice(1).split(' ').join('')] = arrayFromStr[1];
     }
-
   })
-
   return movies;
-}).then(movies => console.log(movies))
+}).then(movies => console.log(movies));
